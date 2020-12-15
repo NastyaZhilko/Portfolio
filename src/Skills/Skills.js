9da7bf1cfff59e873323406b98442933be2be25a
NastyaZhilko <nasten4ik2010@gmail.com>
2020-12-15 23:56:22 +0300

    first commit

---
+++ src/Skills/Skills.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import style from './Skills.module.css';
+import styleContainer from '../Common/Styles/Container.module.css'
+import Skill from "./Skill/Skill";
+
+function Skills() {
+    return (
+        <div className={style.skillsBlock}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <h2 className={style.title}>Skills</h2>
+                <div className={style.skills}>
+                    <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntna '}/>
+                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                    <Skill title={'React'} description={'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
