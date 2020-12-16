ba0621ca1f8bdf50c727e01915db38199ace03e4
NastyaZhilko <nasten4ik2010@gmail.com>
2020-12-16 14:26:57 +0300

    second2 commit

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -10,7 +10,7 @@ function Skills() {
                 <h2 className={style.title}>Skills</h2>
                 <div className={style.skills}>
                     <Skill title={'JS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididuntna...Светик, привет) '}/>
-                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
+                    <Skill title={'CSS'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt magna aliqua Ut enim...'}/>
                     <Skill title={'React'} description={'Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim...'}/>
                 </div>
             </div>
